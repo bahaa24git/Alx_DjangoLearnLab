8239764778e489c4d39a72caad0da1e3598c5dc8
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-09-14 16:41:34 +0300

    Fix: Explicitly import views in urls.py and add routes for books and library detail

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("relationship_app.urls")),
-    # Function-based view
-    path("books/", views.list_books, name="list_books"),
-
-    # Class-based view
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
