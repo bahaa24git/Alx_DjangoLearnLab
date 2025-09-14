f701334e9433e7c8cefb384fd02b157878de071e
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-09-14 16:35:54 +0300

    Task: Add templates (list_books.html, library_detail.html) and update views to render books and library details

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("relationship_app.urls")),
+    # Function-based view
     path("books/", views.list_books, name="list_books"),
+
+    # Class-based view
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 ]
