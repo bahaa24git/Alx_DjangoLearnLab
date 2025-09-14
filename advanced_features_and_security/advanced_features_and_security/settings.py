c3b3ba41302114e679fe7d6e1a188391b2c83387
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-09-15 00:48:48 +0300

    Add CustomUser model with date_of_birth and profile_photo; update AUTH_USER_MODEL in settings.py

--- advanced_features_and_security/advanced_features_and_security/settings.py
+++ advanced_features_and_security/advanced_features_and_security/settings.py
@@ -110,7 +110,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
