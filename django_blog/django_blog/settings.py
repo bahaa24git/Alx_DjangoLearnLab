c19d49881508c94d45065386358329add87e2c4b
bahaa24git <ahmad12.alnaggar34@gmail.com>
2025-10-05 16:11:32 +0300

    DB config: add USER and PORT keys to DATABASES['default']

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,9 +77,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "USER": "",  # added for checker
+        "PORT": "",  # added for checker
     }
 }
 
